bce3dd01df71f42abdd09d313093f302d57a1b24
SamyOubouaziz <soubouaziz@scaleway.com>
2024-07-29 13:51:14 +0200

    docs(gen): update

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,8 +1,4 @@
 const expectedTypes = ['feat', 'fix', 'test', 'build', 'refactor', 'docs'];
-const expectedProducts = ['APS','AUT','BIL','BLS','CPT','ACC','CR','DDX',
-                        'DTO','DOM','EM','ESE','FDB','GEN','GPU','IAM','INS',
-                        'IOT','K8S','LRN','LB','MAS','MNQ','NET','S3','RDB','VPC',
-                        'PGW','RED','SEM','SRV','SDB','SRV','TEM','UAPI','VPC','WH'];
 
 module.exports = {
   extends: ['@commitlint/config-conventional'],
@@ -17,19 +13,10 @@ Commit message must be in the following format:\n
 
 "type(scope): subject"\n
 
-Type must be one of the following:\n
-${expectedTypes.join(', ')} \n
-Scope must be a product acronym: 
+- Scope must be a Scaleway product acronym.\n
 
-Apple Silicon - APS       | Auth - AUT                  | Billing - BIL              | Block storage - BLS
-Cockpit - CPT             | Account - ACC               | Container Registry - CR    | Dedibox - DDX
-Developer tools - DTO     | Domains and DNS - DOM       | Elastic Metal - EM         | Edge Services - ESE
-General - GEN             | GPU - GPU                   | IAM - IAM                  | Instances - INS 
-IoT Hub - IOT             | Kubernetes - K8S            | Learning - LRN             | Load Balancer - LB      
-Managed Services - MAS    | Messaging & Queuing - MNQ   | Network products - NET     | Object Storage - S3     
-PostgreSQL - MySQL - RDB  | Private networks - VPC      | Public Gateway - PGW       | Redis™ - RED
-Secret Manager - SEM      | Serverless Containers - SRV | Serverless Databases - SDB | Serverless Funct - SRV
-Transactional Email - TEM | Users and API - UAPI        | Virtual Private Cloud - VPC| Web hosting - WH
+- Type must be one of the following:\n
+${expectedTypes.join(', ')} \n
 
 Example: 
 "docs(INS): add new documentation page"

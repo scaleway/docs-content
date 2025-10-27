9efd65e1faa85b806d27b4c0fd824c558d5527d0
Rowena Jones <36301604+RoRoJ@users.noreply.github.com>
2025-10-27 15:50:55 +0100

    feat(genapis): add doc for cursor (#5702)
    * feat(genapis): add doc for cursor
    
    * fix(genapis): finish
    
    * Apply suggestions from code review
    
    Co-authored-by: Néda <87707325+nerda-codes@users.noreply.github.com>
    
    * Apply suggestions from code review
    
    ---------
    
    Co-authored-by: Néda <87707325+nerda-codes@users.noreply.github.com>

--- pages/generative-apis/menu.ts
+++ pages/generative-apis/menu.ts
@@ -119,6 +119,10 @@ export const generativeApisMenu = {
           slug: 'integrate-with-n8n'
         },
         {
+          label: 'Integrating Generative APIs with Cursor',
+          slug: 'integrate-with-cursor'
+        },
+        {
           label: 'Integrating Generative APIs with popular AI tools',
           slug: 'integrating-generative-apis-with-popular-tools',
         },

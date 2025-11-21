a0dad98fbc6e9f98129c0441035c8689855c21ce
Valentin Macheret <vmacheret@scaleway.com>
2025-11-21 12:29:50 +0100

    feat(qaas): bootstrap qaas

---
+++ pages/quantum-computing/menu.ts
@@ -0,0 +1,180 @@
+export const serverlessContainersMenu = {
+  items: [
+    {
+      label: 'Overview',
+      slug: '../serverless-containers',
+    },
+    {
+      label: 'Concepts',
+      slug: 'concepts',
+    },
+    {
+      label: 'Quickstart',
+      slug: 'quickstart',
+    },
+    {
+      label: 'FAQ',
+      slug: 'faq',
+    },
+    {
+      items: [
+        {
+          label: 'Create, manage, and delete a namespace',
+          slug: 'create-manage-delete-containers-namespace',
+        },
+        {
+          label: 'Deploy a container',
+          slug: 'deploy-container',
+        },
+        {
+          label: 'Manage a container',
+          slug: 'manage-a-container',
+        },
+        {
+          label: 'Build and push a container image',
+          slug: 'build-push-container-image',
+        },
+        {
+          label: 'Add a custom domain to a container',
+          slug: 'add-a-custom-domain-to-a-container',
+        },
+        {
+          label: 'Modify container privacy',
+          slug: 'modify-container-privacy',
+        },
+        {
+          label:
+            'Create and manage an authentication token from the console',
+          slug: 'create-auth-token-from-console',
+        },
+        {
+          label: 'Secure a container',
+          slug: 'secure-a-container',
+        },
+        {
+          label: 'Use Private Networks',
+          slug: 'use-private-networks',
+        },
+        {
+          label: 'Test a container',
+          slug: 'test-a-container',
+        },
+        {
+          label: 'Monitor container logs and metrics',
+          slug: 'monitor-container',
+        },
+        {
+          label: 'Add a trigger to a container',
+          slug: 'add-trigger-to-a-container',
+        },
+        {
+          label: 'Configure alerts for a container',
+          slug: 'configure-alerts-containers',
+        },
+        {
+          label: 'Delete a container',
+          slug: 'delete-a-container',
+        },
+      ],
+      label: 'How to',
+      slug: 'how-to',
+    },
+    {
+      items: [
+        {
+          label: 'Containers API Reference',
+          slug: 'https://www.scaleway.com/en/developers/api/serverless-containers/',
+        },
+        {
+          label:
+            'Create and deploy a container with the Scaleway API',
+          slug: 'deploy-container-api',
+        },
+        {
+          label:
+            'Create and deploy a container with the Scaleway CLI',
+          slug: 'deploy-container-cli',
+        },
+        {
+          label: 'Migrate images to the Scaleway Registry',
+          slug: 'migrate-external-image-to-scaleway-registry',
+        },
+      ],
+      label: 'API/CLI',
+      slug: 'api-cli',
+    },
+    {
+      items: [
+        {
+          label: 'Serverless overview',
+          slug: 'serverless-overview',
+        },
+        {
+          label: 'Migrating to Serverless Containers',
+          slug: 'migrate-to-serverless-containers'
+        },
+        {
+          label: 'Containers and Private Networks integration',
+          slug: 'containers-private-networks',
+        },
+        {
+          label: 'Containers limitations',
+          slug: 'containers-limitations',
+        },
+        {
+          label: 'Methods to deploy Serverless Containers',
+          slug: 'deploy-container',
+        },
+        {
+          label: 'Containers use cases',
+          slug: 'containers-use-cases',
+        },
+        {
+          label:
+            'Considerations to configure event retention for Queues trigger inputs',
+          slug: 'configure-trigger-inputs',
+        },
+        {
+          label: 'Containers concurrency',
+          slug: 'containers-concurrency',
+        },
+        {
+          label: 'Containers autoscaling',
+          slug: 'containers-autoscaling',
+        },
+        {
+          label: 'Containers billing',
+          slug: 'containers-billing',
+        },
+        {
+          label: 'Containers port parameter',
+          slug: 'port-parameter-variable',
+        },
+        {
+          label: 'Containers sandbox',
+          slug: 'containers-sandbox',
+        },
+        {
+          label: 'Differences between Jobs, Functions and Containers',
+          slug: 'difference-jobs-functions-containers',
+        },
+        {
+          label: 'Cron schedules reference',
+          slug: 'cron-schedules',
+        },
+        {
+          label: 'DNS query rate limiting',
+          slug: 'dns-query-rate-limit',
+        },
+      ],
+      label: 'Additional Content',
+      slug: 'reference-content',
+    },
+    {
+      label: 'Troubleshooting',
+      slug: 'troubleshooting',
+    },
+  ],
+  label: 'Containers',
+  slug: 'serverless-containers',
+}

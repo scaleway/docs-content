c361f046ec765dd2b4bbdf6e77f7787d6813a1da
Rowena <rjones@scaleway.com>
2026-01-22 13:43:23 +0100

    feat(peering): finish first draft of content

--- menu/navigation.ts
+++ menu/navigation.ts
@@ -63,6 +63,7 @@ import { terraformMenu } from "../pages/terraform/menu"
 import { topicsAndEventsMenu } from "../pages/topics-and-events/menu"
 import { transactionalEmailMenu } from "../pages/transactional-email/menu"
 import { vpcMenu } from "../pages/vpc/menu"
+import { vpcPeeringMenu } from "../pages/vpc/menu"
 import { webhostingMenu } from "../pages/webhosting/menu"
 import { quantumComputingMenu } from "../pages/quantum-computing/menu"
 
@@ -211,6 +212,7 @@ export default [
           publicGatewaysMenu,
           siteToSiteVpnMenu,
           vpcMenu,
+          vpcPeeringMenu,
         ],
         label: 'Network',
         category: 'network',
---
+++ pages/vpc-peering/menu.ts
@@ -0,0 +1,50 @@
+export const vpcPeeringMenu = {
+  items: [
+    {
+      label: 'Overview',
+      slug: '../vpc-peering',
+    },
+    {
+      label: 'Concepts',
+      slug: 'concepts',
+    },
+    {
+      label: 'Quickstart',
+      slug: 'quickstart',
+    },
+    {
+      label: 'FAQ',
+      slug: 'faq',
+    },
+    {
+      items: [
+        {
+          label: 'VPC Peering API Reference',
+          slug: 'https://www.scaleway.com/en/developers/api/vpc-peering/',
+        }
+      ],
+      label: 'API/CLI',
+      slug: 'api-cli',
+    },
+    {
+      items: [
+        {
+          label: 'Understanding VPC Peering',
+          slug: 'understanding-svpc-peering',
+        },
+        {
+          label: 'VPC Peering statuses',
+          slug: 'statuses',
+        },
+      ],
+      label: 'Additional Content',
+      slug: 'reference-content',
+    }
+    {
+      label: 'Troubleshooting',
+      slug: 'troubleshooting',
+    },
+  ],
+  label: 'VPC Peering',
+  slug: 'vpc-peering',
+}

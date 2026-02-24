2caa3f9a3a77c83e10ab767ed1499e7916bd38b4
Samy OUBOUAZIZ <soubouaziz@scaleway.com>
2026-02-24 11:59:42 +0100

    doc(obj): update

--- pages/object-storage/menu.ts
+++ pages/object-storage/menu.ts
@@ -59,6 +59,10 @@ export const objectStorageMenu = {
           slug: 'manage-lifecycle-rules',
         },
         {
+          label: 'Enable SSE-ONE',
+          slug: 'enable-sse-one',
+        },
+        {
           label: 'Host healthcare data (HDS)',
           slug: 'host-healthcare-data',
         },

03d9f2ff1a76754f453f1c8968297d6ed6a84d08
Loic-kd <lkhamdy@scaleway.com>
2026-03-23 17:44:19 +0100

    Update menu.ts

--- pages/mailbox/menu.ts
+++ pages/mailbox/menu.ts
@@ -9,14 +9,6 @@ export const mailboxMenu = {
       slug: 'concepts',
     },
     {
-      label: 'Quickstart',
-      slug: 'quickstart',
-    },
-    {
-      label: 'FAQ',
-      slug: 'faq',
-    },
-    {
       items: [
         {
           label: 'Create your Mailbox domain',
@@ -41,25 +33,7 @@ export const mailboxMenu = {
       ],
       label: 'How to',
       slug: 'how-to',
-    },
-    {
-      label: 'API/CLI',
-      slug: 'api-cli',
-    },
-    {
-      items: [
-        {
-          label: 'Mailbox capabilities and limits',
-          slug: 'mailbox-capabilities-and-limits',
-        },
-      ],
-      label: 'Additional Content',
-      slug: 'reference-content',
-    },
-    {
-      label: 'Troubleshooting',
-      slug: 'troubleshooting',
-    },
+    }
   ],
   label: 'Mailbox',
   slug: 'mailbox',

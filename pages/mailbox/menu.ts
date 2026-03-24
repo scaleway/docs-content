c58c99dd6f4021195e3e1c45bf842dc5e386a09c
Loic-kd <lkhamdy@scaleway.com>
2026-03-24 09:26:18 +0100

    feat(mailbox): doc creation MTA-6964 (#6261)
    * feat(mailbox): doc creation
    
    * feat(mailbox): add general navigation and mailbox menu
    
    * feat(mailbox): update menu and index
    
    * feat(mailbox): add concepts page MTA-6965 (#6263)
    
    * feat(mailbox): add concepts page
    
    * Apply suggestions from code review
    
    Co-authored-by: jerjako <contact@jerjako.com>
    
    * feat(mailbox): apply more suggestions from code review
    
    * Apply suggestions from code review
    
    Co-authored-by: Firdevs Arslan <farslan@scaleway.com>
    
    ---------
    
    Co-authored-by: jerjako <contact@jerjako.com>
    Co-authored-by: Firdevs Arslan <farslan@scaleway.com>
    
    * feat(mailbox): add pages to menu
    
    * feat(mailbox): fix delete email account in menu
    
    * feat(mailbox): update index page
    
    * feat(mailbox): add how to create mailbox domain MTA-6966 (#6286)
    
    * feat(mailbox): add how to create mailbox domain MTA-6966
    
    * feat(mailbox): add link to Webmail Access + account protocols
    
    * Apply suggestions from code review
    
    Co-authored-by: Jessica <113192637+jcirinosclwy@users.noreply.github.com>
    Co-authored-by: Firdevs Arslan <farslan@scaleway.com>
    
    * feat(mailbox): fix description
    
    ---------
    
    Co-authored-by: Jessica <113192637+jcirinosclwy@users.noreply.github.com>
    Co-authored-by: Firdevs Arslan <farslan@scaleway.com>
    
    * feat(mailbox): add manage mailbox subscription page MTA-6970 (#6310)
    
    * feat(mailbox): add manage mailbox subscription page
    
    * Apply suggestions from code review
    
    Co-authored-by: Jessica <113192637+jcirinosclwy@users.noreply.github.com>
    
    * feat(mailbox): add pages to menu
    
    * feat(mailbox): update description for how to reactivate subscription
    
    ---------
    
    Co-authored-by: Jessica <113192637+jcirinosclwy@users.noreply.github.com>
    
    * feat(mailbox): add delete mailbox domain page MTA-6971 (#6318)
    
    * feat(mailbox): add delete email account page MTA-6972 (#6317)
    
    * feat(mailbox): add delete email account page
    
    * feat(mailbox): typo fix in description
    
    * feat(mailbox): add beta phase note in index and concepts
    
    * Update menu.ts
    
    * feat(mailbox): correction in Public beta note
    
    * Update pages/mailbox/concepts.mdx
    
    Co-authored-by: SamyOubouaziz <soubouaziz@scaleway.com>
    
    * Update pages/mailbox/index.mdx
    
    Co-authored-by: SamyOubouaziz <soubouaziz@scaleway.com>
    
    ---------
    
    Co-authored-by: jerjako <contact@jerjako.com>
    Co-authored-by: Firdevs Arslan <farslan@scaleway.com>
    Co-authored-by: Jessica <113192637+jcirinosclwy@users.noreply.github.com>
    Co-authored-by: SamyOubouaziz <soubouaziz@scaleway.com>

---
+++ pages/mailbox/menu.ts
@@ -0,0 +1,40 @@
+export const mailboxMenu = {
+  items: [
+    {
+      label: 'Overview',
+      slug: '../mailbox',
+    },
+    {
+      label: 'Concepts',
+      slug: 'concepts',
+    },
+    {
+      items: [
+        {
+          label: 'Create your Mailbox domain',
+          slug: 'create-mailbox-domain',
+        },
+        {
+          label: 'Configure your Mailbox domain',
+          slug: 'configure-mailbox',
+        },
+        {
+          label: 'Manage a Mailbox subscription',
+          slug: 'manage-mailbox-subscription',
+        },
+        {
+          label: 'Delete a Mailbox email account',
+          slug: 'delete-email-account',
+        },
+        {
+          label: 'Delete your Mailbox domain',
+          slug: 'delete-mailbox-domain',
+        },
+      ],
+      label: 'How to',
+      slug: 'how-to',
+    }
+  ],
+  label: 'Mailbox',
+  slug: 'mailbox',
+}

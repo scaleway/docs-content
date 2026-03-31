81eaa71afa8d829e0c41486ee739383decd68a24
Loic KHAMDY <lkhamdy@scaleway.com>
2026-03-31 11:59:46 +0200

    feat(data-orchestrator): doc creation MTA-7002

---
+++ pages/data-orchestrator/menu.ts
@@ -0,0 +1,10 @@
+export const dataOrchestratorMenu = {
+  items: [
+    {
+      label: 'Overview',
+      slug: '../data-orchestrator',
+    },
+  ],
+  label: 'Data Orchestrator',
+  slug: 'data-orchestrator',
+}

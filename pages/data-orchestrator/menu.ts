9d83c4bde4e1888c6b882e4740962ef743ab6859
Loic-kd <lkhamdy@scaleway.com>
2026-04-22 17:18:48 +0200

    feat(data-orchestrator): doc creation MTA-7002 (#6363)
    * feat(data-orchestrator): doc creation MTA-7002
    
    * Apply suggestions from code review
    
    Co-authored-by: Jessica <113192637+jcirinosclwy@users.noreply.github.com>
    
    * feat(data-orchestrator): add concepts page MTA-7008 (#6376)
    
    * feat(data-orchestrator): add concepts page MTA-7008
    
    * feat(data-orchestrator): update concept and index pages
    
    * Apply suggestions from code review
    
    Co-authored-by: Firdevs Arslan <farslan@scaleway.com>
    Co-authored-by: Jessica <113192637+jcirinosclwy@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: Firdevs Arslan <farslan@scaleway.com>
    Co-authored-by: Jessica <113192637+jcirinosclwy@users.noreply.github.com>
    
    * feat(data-orchestrator): add how to create workflow MTA-7013 (#6438)
    
    * feat(data-orchestrator): add how to create workflow MTA-7013
    
    * feat(data-orchestrator): update how to create workflow
    
    * Apply suggestions from code review
    
    Co-authored-by: vanda-scw <villyes@scaleway.com>
    
    * Update tip
    
    ---------
    
    Co-authored-by: vanda-scw <villyes@scaleway.com>
    
    * feat(data-orchestrator): update beta period note
    
    * feat(data-orchestrator): add how to run workflow page MTA-7018 (#6441)
    
    * feat(data-orchestrator): add how to run workflow page MTA-7018
    
    * feat(data-orchestrator): add macro to create workflow
    
    ---------
    
    Co-authored-by: Jessica <113192637+jcirinosclwy@users.noreply.github.com>
    Co-authored-by: Firdevs Arslan <farslan@scaleway.com>
    Co-authored-by: vanda-scw <villyes@scaleway.com>

---
+++ pages/data-orchestrator/menu.ts
@@ -0,0 +1,28 @@
+export const dataOrchestratorMenu = {
+  items: [
+    {
+      label: 'Overview',
+      slug: '../data-orchestrator',
+    },
+    {
+      label: 'Concepts',
+      slug: 'concepts',
+    },
+    {
+      items: [
+        {
+          label: 'Create a workflow',
+          slug: 'create-workflow',
+        },
+        {
+          label: 'Run a workflow',
+          slug: 'run-workflow',
+        },
+      ],
+      label: 'How to',
+      slug: 'how-to',
+    },
+  ],
+  label: 'Data Orchestrator',
+  slug: 'data-orchestrator',
+}

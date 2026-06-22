759b25654e35e1de0705357a07d3f0966dcb4272
Vanda ILLYES <villyes@scaleway.com>
2026-06-22 18:03:39 +0200

    feat(annotations): add menu, index, concepts, placeholder docs MTA-7143

---
+++ pages/annotations/menu.ts
@@ -0,0 +1,60 @@
+export const annotationsMenu = {
+  items: [
+    {
+      label: 'Overview',
+      slug: '../annotations',
+    },
+    {
+      label: 'Concepts',
+      slug: 'concepts',
+    },
+    {
+      label: 'Quickstart',
+      slug: 'quickstart',
+    },
+    {
+      label: 'FAQ',
+      slug: 'faq',
+    },
+    {
+      items: [
+        {
+          label: 'XXX',
+          slug: 'xxx',
+        },
+        {
+          label: 'YYY',
+          slug: 'yyy',
+        },
+        {
+          label: 'ZZZ',
+          slug: 'zzz',
+        },
+      ],
+      label: 'How to',
+      slug: 'how-to',
+    },
+    {
+      items: [
+        {
+          label: 'Annotations API Reference',
+          slug: 'https://www.scaleway.com/en/developers/api/annotations/',
+        },
+      ],
+      label: 'API/CLI',
+      slug: 'api-cli',
+    },
+    {
+      items: [
+        {
+          label: 'Scaleway Resource Names',
+          slug: 'scaleway-resource-names',
+        },
+      ],
+      label: 'Additional Content',
+      slug: 'reference-content',
+    },
+  ],
+  label: 'Annotations',
+  slug: 'annotations',
+}
